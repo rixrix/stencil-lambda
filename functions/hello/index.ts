d7f75e31902d783ff1ec6558aa7f50f69dfcddf3
Richard Sentino <richard@mindginative.com>
2016-08-03 18:26:00 +1200

    chore(example): minor example changes

--- functions/hello/index.ts
+++ functions/hello/index.ts
@@ -1,6 +1,6 @@
-console.log('start simple')
-
-export default function name(e, ctx, cb) {
-    console.log('processing event: %j', e);
+function main(e, ctx, cb) {
+    console.log('processing event!: %j', e);
     cb(null, { hello: e.hello });
 }
+
+export default main;
